2a719aa751b96d5b1e1dd57fba4af46380b8db52
Roman <37734223+RomanKALINACHENKO@users.noreply.github.com>
2021-03-20 01:52:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const app = express()
 
 
 app.use(express.json({ extended: true }))
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    
+   if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','GET, PUT, PATCH, POST, DELETE')
+        return res.status(200).json({})
+   }
+})
 
 app.use('/api/notes', require('./routes/notes.routes'))
 app.use('/api/categories', require('./routes/categories.routes'))
